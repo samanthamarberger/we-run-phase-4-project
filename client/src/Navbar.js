36be57fd11332da74f5b4749a877a4a5c9cbc186
samanthamarberger <slatham04@gmail.com>
2023-08-28 12:52:06 -0600

    troubleshooting Error; invalid hook call.

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NavBar() {
-
+    return (
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+      );    
 }
 
 export default NavBar;
