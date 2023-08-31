a7a5aede0eda5b2c85cc3876e0803ad06e5ae141
samanthamarberger <slatham04@gmail.com>
2023-08-31 11:01:07 -0600

    added login functionality in sessions create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,13 @@ class SessionsController < ApplicationController
 
     #login
     def create
-        
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: {login: "Invalid username or password"}}, status: :unauthorized 
+        end
     end
 
     #logout
