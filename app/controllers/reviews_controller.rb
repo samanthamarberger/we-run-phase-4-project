df934bfec6e9e8db300be0996ef9f8886250a761
samanthamarberger <slatham04@gmail.com>
2023-09-01 10:42:53 -0600

    started controllers for trails and reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,34 @@
 class ReviewsController < ApplicationController
     before_action :authorize
+
+    def index 
+        reviews = current_user.reviews
+        render json: reviews, status: :ok
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+    
+    def current_user 
+        User.find_by(id: session[:user_id])
+    end
+
+    def review_params
+        params.permit(:username, :rating, :review)
+    end
+
+    def authorize
+        return render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
+    end
 end
--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -2,15 +2,44 @@ class TrailsController < ApplicationController
     before_action :authorize
 
     def index 
-        render json: Trail.all, status: :ok
+        trails = current_user.trails
+        render json: trails, status: :ok
     end
 
     def create
+        trail = current_user.trails.create(trail_params)
+        if trail.valid?
+            render json: trail, status: :created
+        else
+            render json: { errors: trail.errors.full_messages }, status: :unprocessable_entity
+        end
     end
 
     def show
+        trail = current_user.trails.find_by(id: params[:id])
+        if trail
+            render json: trail, status: :ok
+        else
+            render json: { errors: "Not found" }, status: :unauthorized
+    end
+
+    def update
     end
 
     def destroy
+    end
+
+    private
+
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+
+    def trail_params
+        params.permit(:trail_name, :description, :location, :difficulty, :trail_image)
+    end
+
+    def authorize
+        return render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
     end
 end
