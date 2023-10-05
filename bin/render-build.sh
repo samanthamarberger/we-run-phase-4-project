f0d837d43645e41c00de226205d2de91567e1c18
samanthamarberger <slatham04@gmail.com>
2023-10-05 17:07:24 -0600

    done

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,7 +9,7 @@ cp -a client/build/. public/
 
 # builds the back end code
 bundle install
-bundle exec rake db:drop
-bundle exec rake db:create
+puts "about to migrate"
 bundle exec rake db:migrate
+puts "about to seed"
 bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
